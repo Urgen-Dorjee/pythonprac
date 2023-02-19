f5e69e45e205ade17c6a4fe6149c043aa0abb5a2
Replit user <>
2023-02-19 23:48:12 +0000

    Added files

--- app.py
+++ app.py
@@ -1,33 +1,32 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-JOBS[
-{
-  id=1,
-  name='Urgen Dorjee',
-location='Ravangla',
-rating='5'
-},
-{
-  id=2,
-  name='Kalsang Nyima',
-location='San Francisco',
-rating='5'
-},
-{
-  id=3,
-  name='Tenzin Norzom',
-location='Albany',
-rating='5'
-}
-]
+datas = [{
+  "id": 1,
+  "name": "John",
+  "location": "New York"
+}, {
+  "id": 2,
+  "name": "Alice",
+  "location": "London"
+}, {
+  "id": 3,
+  "name": "Bob",
+  "location": "Paris"
+}, {
+  "id": 4,
+  "name": "Mary",
+  "location": "Berlin"
+}]
 
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello,Urgen Dorjee</p>"
+  return render_template('home.html',
+                         data=datas,
+                         company_name="Practice Playground")
 
 
 if __name__ == "__main__":
-   app.run(host='0.0.0.0',debug=True)
-  
+  app.run(host='0.0.0.0', debug=True)
