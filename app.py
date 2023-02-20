92dd5a61cab7577d1c2ddcdfdca7c19d3d4ba130
URGEN DORJEE <urgen@UrgenMacPro.local>
2023-02-19 16:18:02 -0800

    Added in for loop

--- app.py
+++ app.py
@@ -2,23 +2,12 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-datas = [{
-  "id": 1,
-  "name": "John",
-  "location": "New York"
-}, {
-  "id": 2,
-  "name": "Alice",
-  "location": "London"
-}, {
-  "id": 3,
-  "name": "Bob",
-  "location": "Paris"
-}, {
-  "id": 4,
-  "name": "Mary",
-  "location": "Berlin"
-}]
+datas = [
+  { "id": 1, "name": "John", "location": "New York"}, 
+  {"id": 2, "name": "Alice", "location": "London"},
+  { "id": 3, "name": "Bob", "location": "Paris" },
+  { "id": 4, "name": "Mary", "location": "Berlin"}
+  ]
 
 
 @app.route("/")
